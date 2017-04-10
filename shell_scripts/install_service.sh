2ec69a23db228cafc0f1e27ce71c9e35b2b20be0
Andrew Herren <andrew.herren@gmail.com>
2017-04-09 20:27:45 -0700

    fixed typos in comment

--- shell_scripts/install_service.sh
+++ shell_scripts/install_service.sh
@@ -3,4 +3,4 @@ cp gpio-init.service /etc/systemd/system/gpio-init.service
 cp office-alarm.service /etc/systemd/system/office-alarm.service
 systemctl daemon-reload
 systemctl enable office-alarm.service
-echo "service installed. Type 'sudo sustemstl start office-alarm' to start the service" 
+echo "service installed. Type 'sudo systemctl start office-alarm' to start the service" 
